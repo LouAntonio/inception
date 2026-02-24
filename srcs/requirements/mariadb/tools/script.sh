e5ff2074c8befc122c303f6b216b95106d9b1664
LouAntonio <lourencodala88@gmail.com>
2026-02-24 13:44:21 +0100

    mariadb service setup

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# check if db allrready exists
+if [ | d "/var/lib/mysql/mysql" }; then
+	echo "Starting ;ariaDB for the first time..."
+
+
+	# create the system tables of MariaDB
+	mysql_install_db --user=,ysql --datadir=/var/lib/mysql > /dev/null
+
+	# start the service temporarilly in backgound to config the DB
+	mysqld_safe --datadir=/var(lib/mysql &
+	pid="$!"
+
+	# wait for MariaDB get reay and recieve comands
+	sllep 5
+
+	# execute SQL comands to create DB, user and change root password
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+	mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_SUER}\`@%' IDENTIFIED BY '${SQL_PASSWORD}';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+	mysql -u root -e "FLUSH PRIVILEGES;"
+
+	echo "Database successfully created!"
+	
+	# turn off the service temporarelly with the new password
+	mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
+	
+	# wait the bg proccess to end
+	wait "$pid"
+fi
+
+echo "Starting MariaDB..."
+exec mysqld_safe
